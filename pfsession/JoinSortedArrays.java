b4131a3244ce145a5f05f297d6adc21bcf8baa2d
chetansharma-gitHub <sharmachetan187@gmail.com>
2017-07-14 14:51:58 +0530

    first commit

---
+++ pfsession/JoinSortedArrays.java
@@ -0,0 +1,35 @@
+
+package pfsession;
+
+public class JoinSortedArrays {
+
+    public int[] join(int a[], int asize, int b[], int bsize, int c[]) {
+        int i = 0, j = 0, k = 0;
+        /*
+         * here this following code is comparing both arrays and storing values in third array named as c  
+         */
+        
+        while (i < asize && j < bsize) {
+            if (a[i] < b[j]) {
+                c[k++] = a[i++];
+            } else {
+                c[k++] = b[j++];
+            }
+        }
+        /*
+         * when one of these both is traced fully ,we are coping as it is the remaining array elements  
+         */
+        if (i == asize && j != bsize) {
+            while (j < bsize) {
+                c[k++] = b[j++];
+            }
+        } else if (i != asize && j == bsize) {
+            while (j < bsize) {
+                c[k++] = a[i++];
+            }
+
+        }
+        return c;
+    }
+
+}
