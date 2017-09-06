b09c6084ffe9c34cec4b826eed946ea6cef19b60
chetansharma-gitHub <sharmachetan187@gmail.com>
2017-09-06 14:58:04 +0530

    DBMS-Session2-Assignment

--- DBMS-Session2-Assignment/Assignment4/DB and table creation.sql
+++ DBMS-Session2-Assignment/Assignment4/DB and table creation.sql
@@ -5,18 +5,17 @@ USE city_zip_code;
 CREATE TABLE states(
 state_id INT AUTO_INCREMENT UNIQUE,
 state_name VARCHAR(30),
-PRIMARY KEY(state_name)
+PRIMARY KEY(state_id)
 );
 
 CREATE TABLE cities(
 city_id INT AUTO_INCREMENT UNIQUE,
-zip_code CHAR(6) NOT NULL,
+zip_code INT(6) NOT NULL,
 state_id INT,
 city_name VARCHAR(30) NOT NULL,
 PRIMARY KEY(zip_code),
-CONSTRAINT cities_id_constraint FOREIGN KEY(state_id) REFERENCES states (state_id)
+CONSTRAINT cities_table_state_id_constraint FOREIGN KEY(state_id) REFERENCES states (state_id)
 );
-
 
 
 
