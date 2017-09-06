dd66a719cf23aad3f899f10203ee0f835737b584
chetansharma-gitHub <sharmachetan187@gmail.com>
2017-09-06 18:14:00 +0530

    DS-Session3-Assignment

--- DBMS-Session2-Assignment/Assignment4/DataRetrivelQueries.sql
+++
@@ -1,7 +0,0 @@
-USE city_zip_code;
-
-/* query for retriving zip_code,city_name,state_name*/
-SELECT zip_code,city_name,state_name
-FROM states AS s,cities AS c
-WHERE s.state_id=c.state_id
-ORDER BY s.state_name,c.city_name;
