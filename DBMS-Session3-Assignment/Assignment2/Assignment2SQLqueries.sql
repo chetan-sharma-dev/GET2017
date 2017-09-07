6fea07e7d110d4c65ad3e6a1323f4a2529b2db6e
chetansharma-gitHub <sharmachetan187@gmail.com>
2017-09-07 15:25:24 +0530

    changes done

--- DBMS-Session3-Assignment/Assignment2/Assignment2SQLqueries.sql
+++ DBMS-Session3-Assignment/Assignment2/Assignment2SQLqueries.sql
@@ -1,6 +1,6 @@
 USE library_information_system;
 /*1*/
-SELECT S.subject_name, COUNT(S.subject_name) AS NO_Of_Books_Purchased
+SELECT S.subject_name, S.subject_id,COUNT(S.subject_name) AS NO_Of_Books_Purchased
 FROM books AS B
 JOIN titles AS T
 ON B.title_id=T.title_id
