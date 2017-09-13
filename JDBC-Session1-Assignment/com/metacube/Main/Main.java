446066ed58d3e4cdb8dfd0256258d5c2bad129b8
chetansharma-gitHub <sharmachetan187@gmail.com>
2017-09-13 17:52:13 +0530

    JDBC-Session1-Assignment

---
+++ JDBC-Session1-Assignment/com/metacube/Main/Main.java
@@ -0,0 +1,10 @@
+package com.metacube.Main;
+
+import com.metacube.controller.Controller;
+
+public class Main {
+	public static void main(String[] args) {
+		Controller controllerObject=new Controller();
+		controllerObject.showMenuTakeInputAndCallMethods();
+	}
+}
