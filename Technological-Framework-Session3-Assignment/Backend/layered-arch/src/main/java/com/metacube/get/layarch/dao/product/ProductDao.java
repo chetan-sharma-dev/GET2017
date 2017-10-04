f0cd223a0ac505eb893046bbb0410b596735f0a6
chetansharma-gitHub <sharmachetan187@gmail.com>
2017-10-04 12:06:16 +0530

    Technological-Framework-Session3-Assignment

---
+++ Technological-Framework-Session3-Assignment/Backend/layered-arch/src/main/java/com/metacube/get/layarch/dao/product/ProductDao.java
@@ -0,0 +1,10 @@
+package com.metacube.get.layarch.dao.product;
+
+
+import com.metacube.get.layarch.dao.AbstractDao;
+import com.metacube.get.layarch.model.Product;
+
+public interface ProductDao extends AbstractDao<Product, Integer>
+{
+
+}
